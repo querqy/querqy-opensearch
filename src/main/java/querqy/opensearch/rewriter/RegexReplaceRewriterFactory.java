254d84bc739e8458d60d69d6986a57aaf30eaaf7
René Kriegler <post@rene-kriegler.com>
2026-06-12 12:46:57 +0200

    Add RegexReplaceRewriter. Implements #141

---
+++ src/main/java/querqy/opensearch/rewriter/RegexReplaceRewriterFactory.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package querqy.opensearch.rewriter;
+
+import org.opensearch.OpenSearchException;
+import org.opensearch.index.shard.IndexShard;
+import querqy.opensearch.ConfigUtils;
+import querqy.opensearch.OpenSearchRewriterFactory;
+import querqy.rewrite.RewriterFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class RegexReplaceRewriterFactory  extends OpenSearchRewriterFactory {
+
+    private static final boolean DEFAULT_IGNORE_CASE = true;
+
+    public static final String PROP_RULES = "rules";
+    public static final String PROP_IGNORE_CASE = "ignoreCase";
+
+    private querqy.rewrite.replace.RegexReplaceRewriterFactory delegate;
+
+    public RegexReplaceRewriterFactory(final String rewriterId) {
+        super(rewriterId);
+    }
+
+    @Override
+    public void configure(final Map<String, Object> config) throws OpenSearchException {
+
+        final String rules = (String) config.get(PROP_RULES);
+        final InputStreamReader rulesReader = new InputStreamReader(
+                new ByteArrayInputStream(rules.getBytes(StandardCharsets.UTF_8)),
+                StandardCharsets.UTF_8);
+
+        final boolean ignoreCase = ConfigUtils.getArg(config, PROP_IGNORE_CASE, DEFAULT_IGNORE_CASE);
+
+        try {
+            delegate = new querqy.rewrite.replace.RegexReplaceRewriterFactory(rewriterId, rulesReader, ignoreCase);
+        } catch (final IOException e) {
+            throw new OpenSearchException(e);
+        }
+    }
+
+    @Override
+    public List<String> validateConfiguration(final Map<String, Object> config) {
+        final String rules = (String) config.get(PROP_RULES);
+        if (rules == null) {
+            return Collections.singletonList("Property " + PROP_RULES + " not configured");
+        }
+
+        final InputStreamReader rulesReader = new InputStreamReader(
+                new ByteArrayInputStream(rules.getBytes(StandardCharsets.UTF_8)),
+                StandardCharsets.UTF_8);
+
+        final boolean ignoreCase = ConfigUtils.getArg(config, PROP_IGNORE_CASE, DEFAULT_IGNORE_CASE);
+
+        try {
+            new querqy.rewrite.replace.RegexReplaceRewriterFactory(rewriterId, rulesReader, ignoreCase);
+        } catch (final IOException e) {
+            return Collections.singletonList("Cannot create rewriter: " + e.getMessage());
+        }
+
+        return List.of();
+    }
+
+    @Override
+    public RewriterFactory createRewriterFactory(final IndexShard indexShard) throws OpenSearchException {
+        return delegate;
+    }
+}
---
+++ src/test/java/querqy/opensearch/rewriter/RegexReplaceRewriterIntegrationTest.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package querqy.opensearch.rewriter;
+
+import org.opensearch.action.search.SearchRequestBuilder;
+import org.opensearch.action.search.SearchResponse;
+import org.opensearch.search.SearchHits;
+import querqy.opensearch.QuerqyProcessor;
+import querqy.opensearch.query.MatchingQuery;
+import querqy.opensearch.query.QuerqyQueryBuilder;
+import querqy.opensearch.query.Rewriter;
+import querqy.opensearch.rewriterstore.PutRewriterAction;
+import querqy.opensearch.rewriterstore.PutRewriterRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import static java.util.Collections.singletonList;
+
+public class RegexReplaceRewriterIntegrationTest extends AbstractRewriterIntegrationTest {
+
+
+    public void testRegexReplaceRewriterRules() throws ExecutionException, InterruptedException {
+        indexDocs(
+                doc("id", "1", "field1", "162x12"),
+                doc("id", "2", "field1", "162 x 12")
+        );
+
+        final Map<String, Object> content = new HashMap<>();
+        content.put("class", RegexReplaceRewriterFactory.class.getName());
+
+        final Map<String, Object> config = new HashMap<>();
+        config.put("rules", "(\\d+) ?x ?(\\d+) => ${1}x${2}");
+        content.put("config", config);
+
+        final PutRewriterRequest request = new PutRewriterRequest("regex_replace", content);
+
+        client().execute(PutRewriterAction.INSTANCE, request).get();
+
+        QuerqyQueryBuilder querqyQuery = new QuerqyQueryBuilder(getInstanceFromNode(QuerqyProcessor.class));
+        querqyQuery.setRewriters(singletonList(new Rewriter("regex_replace")));
+        querqyQuery.setMatchingQuery(new MatchingQuery("162x 12"));
+        querqyQuery.setMinimumShouldMatch("1");
+        querqyQuery.setQueryFieldsAndBoostings(singletonList("field1"));
+
+        SearchRequestBuilder searchRequestBuilder = client().prepareSearch(getIndexName());
+        searchRequestBuilder.setQuery(querqyQuery);
+
+        SearchResponse response = client().search(searchRequestBuilder.request()).get();
+        SearchHits hits = response.getHits();
+
+        assertEquals(1L, hits.getTotalHits().value());
+        assertEquals("1", hits.getAt(0).getSourceAsMap().get("id"));
+
+    }
+
+}
